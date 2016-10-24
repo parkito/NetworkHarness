6f92f4144f999bfcca9c7d464a0d4f7e1e65f4c0
parkito <artyom-karnov@yandex.ru>
2016-10-24 23:36:29 +0300

    24.10.2016

--- Server/src/main/java/Sec.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Artyom Karnov on 10/24/16.
- * artyom-karnov@yandex.ru
- **/
-public class Sec {
-    public static void main(String[] args) {
-
-    }
-}
--- simple-service-webapp/src/main/java/com/example/FtoCService.java
+++ simple-service-webapp/src/main/java/com/example/FtoCService.java
@@ -1,3 +1,5 @@
+package com.example;
+
 /**
  * Created by Artyom Karnov on 10/24/16.
  * artyom-karnov@yandex.ru
