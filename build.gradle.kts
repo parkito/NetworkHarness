96630812f5965809f6a5dce6aceeea92366a1dc0
parkito <artyom-karnov@yandex.ru>
2019-05-27 20:01:12 +0300

    Add rx structure

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+group = "ru.siksmfp.rx.play"
+version = "0.1.1"
+
+val appArchiveName = "rx-$version"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.21"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+//application {
+//    mainClassName = "samples.HelloWorldKt"
+//}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile("org.springframework:spring-context:5.1.6.RELEASE")
+    compile("javax.annotation:javax.annotation-api:1.3.2")
+    compile("io.reactivex.rxjava2:rxjava:2.2.8")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.3.20")
+    testCompile("org.junit.jupiter:junit-jupiter-engine:5.4.1")
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+val jar by tasks.getting(Jar::class) {
+    archiveName = "$appArchiveName.jar"
+    into("META-INF") {
+    }
+}
---
+++ src/test/kotlin/RxSimpleTest.kt
@@ -0,0 +1,14 @@
+import io.reactivex.Observable
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+class RxSimpleTest {
+    @Test
+    fun returnAValue() {
+        var result = ""
+        val observer = Observable.just("Hello") // provides datea
+        observer.subscribe { s -> result = s } // Callable as subscriber
+        assertTrue(result == "Hello")
+    }
+}
