2c47b2e980088f296d6689f848829ab9645fb8f5
parkito <artyom-karnov@yandex.ru>
2019-12-10 00:01:57 -0700

    Improve decorator pattern

--- src/main/java/ru/siksmfp/rx/play/handler/impl/PrintableHandler.java
+++ src/main/java/ru/siksmfp/rx/play/handler/impl/PrintableHandler.java
@@ -1,22 +1,21 @@
 package ru.siksmfp.rx.play.handler.impl;
 
+import ru.siksmfp.rx.play.handler.api.DecoratedHandler;
 import ru.siksmfp.rx.play.handler.api.Handler;
 
 import java.io.IOException;
 
-public class PrintableHandler<S> implements Handler<S> {
-
-    private Handler<S> handler;
+public class PrintableHandler<S> extends DecoratedHandler<S> {
 
     public PrintableHandler(Handler<S> handler) {
-        this.handler = handler;
+        super(handler);
     }
 
     @Override
     public void handle(S s) throws IOException {
         try {
             System.out.println("Connecting socket " + s);
-            handler.handle(s);
+            super.handle(s);
         } finally {
             System.out.println("Disconnecting socket " + s);
         }
